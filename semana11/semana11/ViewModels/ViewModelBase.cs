cf035352fb99d434b7662a2d86418647f96981ef
karoline bustinza <karoline.bustinza@tecsup.edu.pe>
2021-05-17 17:44:25 -0500

    first commit

---
+++ semana11/semana11/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace semana11.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(
+            [CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler !=null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+    
+}
---
+++ semana11/semana11/Views/Operations.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana11.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operations : ContentPage
+    {
+        public Operations()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.OperationsViewModel();
+
+        }
+    }
+}
